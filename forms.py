d4456b19a1cb75b2a48d7cfe32c587242ab88113
Tiania <tiania.chan@gmail.com>
2021-04-07 21:48:51 -0700

    select fields added to keep users  from  adding fields not in database for cleaner data

--- forms.py
+++ forms.py
@@ -1,13 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField
+from wtforms import StringField, IntegerField, SelectField, SubmitField
 
 class AddForm(FlaskForm):
 
     name = StringField('Name of Puppy:')
-    size = StringField('Size of Puppy (small, medium, large):')
+    # size = StringField('Size of Puppy (small, medium, large):')
+    size = SelectField(u'Pick a size:',
+                          choices=[('sm', 'small'), ('med', 'medium'),
+                                   ('lg', 'large')])
     age = IntegerField('Age of Puppy (years):')
     breed = StringField('Breed of Puppy:')
-    activity_mode = StringField('Activity mode (couch potato, regular, hyper):')
+    # activity_mode = StringField('Activity mode (couch potato, regular, hyper):')
+    activity_mode = SelectField(u'Choose the activity mode:',
+                          choices=[('cp', 'couch potato'), ('rg', 'regular'),
+                                   ('hy', 'hyper')])
     submit = SubmitField('Add Puppy')
 
 class AddOwnerForm(FlaskForm):
